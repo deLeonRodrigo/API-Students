e11925d251f57a26b738477ec7b9e609235c8aa3
Rodrigo de Leon <rodri230998@gmail.com>
2018-10-02 01:59:56 -0500

    added delete and update functions

--- Server/dbconnection.js
+++ Server/dbconnection.js
@@ -1,8 +1,20 @@
 const MongoClient = require('mongodb').MongoClient;
 const urlDB = "mongodb://localhost:27017";
 var assert = require('assert');
+var ObjectID = require('mongodb').ObjectID; 
 var db;
 var _students;
+
+function deleteStudent(data){
+    MongoClient.connect(urlDB, function(err, db) {
+        db = db.db('students');
+        db.collection('students').deleteOne(
+            {'_id': ObjectID(data._Id)},
+            (err, result) => {
+            if (err) return console.log(err);
+        })
+    })
+}
 
 function openConnection(){
     MongoClient.connect(urlDB, function(err, db) {
@@ -21,6 +33,17 @@ function insert(data){
     })
 }
 
+function update(data){
+    MongoClient.connect(urlDB, function(err, db) {
+        db = db.db('students');
+        db.collection('students').update(
+            {'_id': ObjectID(data._Id)} ,
+            data, (err, result) => {
+            if (err) return console.log(err);
+        })
+    })
+}
+
 function closeConnection(){
     db.close();
 }
@@ -34,11 +57,12 @@ function get(){
         })
     })
 }
-
 module.exports.openConnection = openConnection;
 module.exports.closeConnection = closeConnection;
 module.exports.insert = insert;
 module.exports.get = get;
+module.exports.delete = deleteStudent;
+module.exports.update = update;
 module.exports.students = () => {
     return _students;
 };
